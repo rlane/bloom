b4e35be30e783bcad81a993ff4ce135f8901a503
Rich Lane <rlane@club.cc.cmu.edu>
2009-01-28 19:15:28 -0500

    more elaborate test

--- test.c
+++ test.c
@@ -3,20 +3,58 @@
 #include <string.h>
 #include "bloom.h"
 
+bloom_t *b;
+
+static void insert(char *key)
+{
+	bloom_insert(b, key, strlen(key));
+}
+
+static void check(char *key)
+{
+	if (bloom_check(b, key, strlen(key)) != 1) {
+		printf("did not find key %s\n", key);
+	}
+}
+
+static void checknot(char *key)
+{
+	if (bloom_check(b, key, strlen(key)) != 0) {
+		printf("unexpectedly found key %s\n", key);
+	}
+}
+
+static void nic(char *key)
+{
+	checknot(key);
+	insert(key);
+	check(key);
+}
+
+static void ic(char *key)
+{
+	insert(key);
+	check(key);
+}
+
 int main(int argc, char** argv)
 {
-	bloom_t *b;
+	char *key;
 	char *fn = "/tmp/test_bloom_filter";
 
 	bloom_create(fn, 4096 * 128);
 	b = bloom_open(fn);
 	printf("loaded bloom filter of size %d\n", b->len);
 
-	char *key = "foo1234567";
-	bloom_insert(b, key, strlen(key));
-	if (bloom_check(b, key, strlen(key)) != 1) printf("lookup of %s failed\n", key);;
+	nic("foo1234567");
+	ic("foo1234567");
+	nic("bazhydyusfhkjds");
+	ic("bazhydyusfhkjds");
+	check("foo1234567");
 
 	bloom_close(b);
+
+	printf("tests passed\n");
 
 	return 0;
 }
