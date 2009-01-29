87ff03c0d08cf6d76b76048e55f706a6cfc5aa40
Rich Lane <rlane@club.cc.cmu.edu>
2009-01-29 11:29:55 -0500

    expand test-big checks

--- test-big.c
+++ test-big.c
@@ -14,9 +14,10 @@ int main(int argc, char** argv)
 	char *fn = "/tmp/test_bloom_filter";
 	bloom_t *b;
 	uint8_t *keys;
-	const int n = 10000;
+	const int n = 1000000;
 	const int key_len = 20;
 	int i;
+	int stat_before = 0;
 
 	fprintf(stderr, "allocating keys\n");
 	keys = malloc(n * key_len);
@@ -35,12 +36,25 @@ int main(int argc, char** argv)
 	fprintf(stderr, "inserting\n");
 	for (i=0; i<n; i++) {
 		uint8_t *k = keys + (i*key_len);
+		int found = bloom_check(b, k, key_len);
+		stat_before += found;
 		bloom_insert(b, k, key_len);
+	}
+
+	fprintf(stderr, "checking\n");
+	for (i=0; i<n; i++) {
+		uint8_t *k = keys + (i*key_len);
+		int found = bloom_check(b, k, key_len);
+		if (!found) {
+			fprintf(stderr, "missing key %d\n", i);
+			abort();
+		}
 	}
 
 	bloom_close(b);
 
 	printf("tests passed\n");
+	printf("before=%d\n", stat_before);
 
 	return 0;
 }
