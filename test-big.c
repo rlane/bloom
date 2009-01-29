18607d6900504cc8d21a33996ede080581866adb
Rich Lane <rlane@club.cc.cmu.edu>
2009-01-29 11:07:09 -0500

    test-big random data

--- test-big.c
+++ test-big.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/rand.h>
 #include "bloom.h"
+
+void *fill_random(uint8_t *p, int n)
+{
+	RAND_pseudo_bytes(p, n);
+}
 
 int main(int argc, char** argv)
 {
@@ -10,14 +16,26 @@ int main(int argc, char** argv)
 	uint8_t *keys;
 	const int n = 10000;
 	const int key_len = 20;
+	int i;
 
-	bloom_create(fn);
-	b = bloom_open(fn);
-
+	fprintf(stderr, "allocating keys\n");
 	keys = malloc(n * key_len);
 	if (keys == NULL) {
 		fprintf(stderr, "failed to allocate key array\n");
 		abort();
+	}
+
+	fprintf(stderr, "generating keys\n");
+	fill_random(keys, n * key_len);
+
+	fprintf(stderr, "creating filter\n");
+	bloom_create(fn);
+	b = bloom_open(fn);
+
+	fprintf(stderr, "inserting\n");
+	for (i=0; i<n; i++) {
+		uint8_t *k = keys + (i*key_len);
+		bloom_insert(b, k, key_len);
 	}
 
 	bloom_close(b);
