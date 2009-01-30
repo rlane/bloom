6d9f7b2e5b39edadfae3cfc911b3ff48d908052f
Rich Lane <rlane@club.cc.cmu.edu>
2009-01-30 02:34:11 -0500

    replace md5 with murmur

--- bloom.c
+++ bloom.c
@@ -6,7 +6,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <openssl/md5.h>
 #include "bloom.h"
 
 #define FILE_SIZE (1<<25)
@@ -17,6 +16,8 @@
 #define NUM_IDXS 8
 
 typedef uint16_t idx_t;
+
+unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed );
 
 int bloom_create(const char *fn)
 {
@@ -95,15 +96,13 @@ fail_malloc:
 
 static void key2idxs(const uint8_t *key, size_t key_len, idx_t *idxs, int n)
 {
-	uint8_t md5[MD5_DIGEST_LENGTH];
-	MD5_CTX c;
-	MD5_Init(&c);
-	MD5_Update(&c, key, key_len);
-	MD5_Final(md5, &c);
-
 	int i;
-	for (i = 0; i < n; i++) {
-		idxs[i] = ((idx_t*)md5)[i];
+	int hash = 0xdeadbeef;
+	for (i = 0; i < NUM_IDXS; i+=2) {
+		hash = MurmurHash2(key, key_len, hash);
+		idxs[i] = hash;
+		if (i + 1 < NUM_IDXS)
+			idxs[i+1] = hash >> 16;
 	}
 }
 
@@ -157,4 +156,69 @@ void bloom_close(bloom_t *b)
 	munmap(b->map, FILE_SIZE);
 	close(b->fd);
 	free(b);
+}
+
+//-----------------------------------------------------------------------------
+// MurmurHash2, by Austin Appleby
+
+// Note - This code makes a few assumptions about how your machine behaves -
+
+// 1. We can read a 4-byte value from any address without crashing
+// 2. sizeof(int) == 4
+
+// And it has a few limitations -
+
+// 1. It will not work incrementally.
+// 2. It will not produce the same results on little-endian and big-endian
+//    machines.
+
+unsigned int MurmurHash2 ( const void * key, int len, unsigned int seed )
+{
+	// 'm' and 'r' are mixing constants generated offline.
+	// They're not really 'magic', they just happen to work well.
+
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+
+	// Initialize the hash to a 'random' value
+
+	unsigned int h = seed ^ len;
+
+	// Mix 4 bytes at a time into the hash
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	while(len >= 4)
+	{
+		unsigned int k = *(unsigned int *)data;
+
+		k *= m; 
+		k ^= k >> r; 
+		k *= m; 
+		
+		h *= m; 
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+	
+	// Handle the last few bytes of the input array
+
+	switch(len)
+	{
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0];
+	        h *= m;
+	};
+
+	// Do a few final mixes of the hash to ensure the last few
+	// bytes are well-incorporated.
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
 }
